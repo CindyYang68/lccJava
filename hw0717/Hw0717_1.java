3e41746be9f6bbcd899b904bbc74abb61e07da58
CindyYang68 <110874267+CindyYang68@users.noreply.github.com>
2022-08-20 10:41:32 +0900

    Add files via upload

---
+++ hw0717/Hw0717_1.java
@@ -0,0 +1,15 @@
+package hw.hw0717;
+
+public class Hw0717_1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=9;i++) {
+			for(int x=1;x<=9;x++) {
+				System.out.print(x+"*"+i+"="+i*x+"\t");
+			}
+			System.out.println();
+		}
+		
+	}
+}
---
+++ hw0717/Hw0717_2.java
@@ -0,0 +1,21 @@
+package hw.hw0717;
+
+public class Hw0717_2 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++) {
+			for(int x=1;x<=i;x++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		for(int i=4;i>=1;i--) {
+			for(int x=1;x<=i;x++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
